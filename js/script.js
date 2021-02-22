41fdd7b62064f4e6984545a7ec201ee6c085cd52
Roman Kotelnikov <kotelnikovrm@gmail.com>
2021-02-23 03:27:58 +0800

    update function check_word

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var img = document.querySelector(".article__img");
 var btn = document.querySelector(".article__btn");
 var input = document.querySelector(".article__input");
+var result = document.querySelector(".article__result");
 var index_catalog = 0;
 var index_word = 0;
 
@@ -31,18 +32,56 @@ var people = {
     ]
 };
 
-var words = [people];
+animals = {
+    imgs: [
+        "img/animals/dog.jpg",
+        "img/animals/cat.jpg",
+        "img/animals/hamster.jpg",
+        "img/animals/turtle.jpg"
+    ],
+    words: [
+        "dog",
+        "cat",
+        "hamster",
+        "turtle"
+    ]
+};
+
+fruits = {
+    imgs: [
+        "img/fruits/apple.jpg",
+        "img/fruits/orange.jpg",
+        "img/fruits/banana.jpg"
+    ],
+    words: [
+        "apple",
+        "orange",
+        "banana"
+    ]
+};
+
+var words = [people, animals, fruits];
 
 function random__img(arr) {
+    //выбор случайного массива из words
     arr_random = Math.floor(Math.random() * arr.length);
     index_catalog = arr_random;
+
+    //выбор изображения
     img_random = Math.floor(Math.random() * arr[arr_random].imgs.length);
     index_word = img_random;
+
     input.value = "Введите по английски что видите";
     return img.src = arr[arr_random].imgs[img_random];
 }
 random__img(words);
 
 function check_word() {
-    console.log(input.value == words[index_catalog].words[index_word] ? true : false);
+    //return true or false
+    if (input.value.toLowerCase() == words[index_catalog].words[index_word]) {
+        result.src = "img/yes.png";
+    }
+    else {
+        result.src = "img/none.png";
+    }
 }
