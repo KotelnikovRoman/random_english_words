a89118154b867715ee11292c5b803e4344b83c52
Roman Kotelnikov <kotelnikovrm@gmail.com>
2021-02-27 11:45:43 +0800

    add function floor, update obj transport, update style

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ var result = document.querySelector(".gaming__img-result");
 var index_catalog = 0;
 var index_word = 0;
 
+//словарь
 var people = {
     imgs: [
         "img/people/man.jpg",
@@ -61,11 +62,51 @@ fruits = {
     ]
 };
 
-var words = [people, animals, fruits];
+transport = {
+    imgs: [
+        "img/transport/plane.jpg",
+        "img/transport/helicopter.jpg",
+        "img/transport/bus.jpg",
+        "img/transport/trolleybus.jpg",
+        "img/transport/tram.jpg",
+        "img/transport/train.jpg",
+        "img/transport/car.jpg",
+        "img/transport/motorcycle.jpg",
+        "img/transport/moped.jpg",
+        "img/transport/bicycle.jpg",
+        "img/transport/boat.jpg",
+        "img/transport/steamboat.jpg",
+        "img/transport/jet ski.jpg",
+        "img/transport/submarine.jpg"
+    ],
+    words: [
+        "plane",
+        "helicopter",
+        "bus",
+        "trolleybus",
+        "tram",
+        "train",
+        "car",
+        "motorcycle",
+        "moped",
+        "bicycle",
+        "boat",
+        "steamboat",
+        "jet ski",
+        "submarine"
+    ]
+}
+
+var words = [people, animals, fruits, transport];
+
+function floor(x) {
+    return Math.floor(Math.random() * (x.length));
+}
 
 function random__img(arr) {
     //выбор случайного массива из words
-    arr_random = Math.floor(Math.random() * arr.length);
+    //arr_random = Math.floor(Math.random() * arr.length);
+    arr_random = floor(arr);
     index_catalog = arr_random;
 
     //выбор изображения
@@ -79,10 +120,18 @@ random__img(words);
 
 function check_word() {
     //return true or false
+    result.style.display = "block";
     if (input.value.toLowerCase() == words[index_catalog].words[index_word]) {
         result.src = "img/yes.png";
     }
     else {
         result.src = "img/none.png";
     }
+}
+
+function gaming() {
+    /**
+     * запуск игры и
+     * отсчёт времени
+     */
 }
